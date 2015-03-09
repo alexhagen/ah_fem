e1d987ebd470fa5e1069fd6261f61e8ab2e0fb13
Alex Hagen <ahagen@purdue.edu>
2015-03-09 01:08:50 -0400

    still importing boundary conditions - abaqus is really clunky about this

--- import_bcf.m
+++ import_bcf.m
@@ -2,15 +2,35 @@ function [bcf] = import_bcf(filename,mesh)
     % Isolate the Assembly Portion of the .inp file
     assm = isolate_inp_portion('ASSEMBLY',filename);
     % find each set name
-    [start_ind_nset,stop_ind_nset] = regexp(assm,...
+    start_ind_nset = regexp(assm,...
         '(\*Nset,\snset\=[A-Za-z1-9\-]*)');
-    [start_ind_elset,stop_ind_elset] = regexp(assm,...
+    start_ind_elset = regexp(assm,...
         '(\*Elset,\selset\=[A-Za-z1-9\-]*)');
     % for each set name
-        for i = 1:numel(start_ind_nset)
-            %find the name
-            %find the elements
-        end
+    for i = 1:numel(start_ind_nset)
+        %find the set name
+        set = sscanf(assm(start_ind_nset(i):end),...
+            '*Nset, nset=%s,');
+        sets(i).name = set(1:end-1);
+        %find the next newline
+        next = regexp(assm(start_ind_nset(i):end), '[\f\n\r]');
+        %find the nodes corresponding to this
+        s = assm(start_ind_nset(i)+next(1):end);
+        last = regexp(assm(start_ind_nset(i)+next(1):end),'(\*)');
+        s = regexprep(assm(start_ind_nset(i)+next(1):...
+            start_ind_nset(i)+next(1)+last(1)-2),'[\f\n\r]',' ');
+        s = regexprep(s,',',' ');
+        sets(i).nodes = sscanf(s,'%f ');
+        %find the elements
+        next = regexp(assm(start_ind_elset(i):end), '[\f\n\r]');
+        %find the nodes corresponding to this
+        s = assm(start_ind_elset(i)+next(1):end);
+        last = regexp(assm(start_ind_elset(i)+next(1):end),'(\*)');
+        s = regexprep(assm(start_ind_elset(i)+next(1):...
+            start_ind_elset(i)+next(1)+last(1)-2),'[\f\n\r]',' ');
+        s = regexprep(s,',',' ');
+        sets(i).els = sscanf(s,'%f ')
+    end
     % Isolate the Boundary Conditions Portion of the .inp file
     bcstr = isolate_inp_portion('BOUNDARY CONDITIONS',filename);
     start_ind_name = findstr('** Name: ',bcstr);
