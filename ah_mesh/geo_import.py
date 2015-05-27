172f8797ee6a3820d295b2c0e6c3956930ea2c6d
Alex Hagen <ahagen@purdue.edu>
2015-05-27 09:32:00 -0400

    writing the structure of the meshing module

---
+++ ah_mesh/geo_import.py
@@ -0,0 +1,27 @@
+import numpy as np 
+
+class geo_import(object):
+	def __init__(filename):
+		self.filename = filename;
+		if '.x3d' is in self.filename:
+			# we have a 3d shape if we have an x3d file
+			self.dim = 3;
+			self.import_3d();
+		elif '.svg' is in self.filename:
+			# we have a 2d shape if we have an svg file
+			self.dim = 2;
+			self.import_2d();
+		else:
+			## add a filename error here
+			pass;
+			
+	def import_2d():
+		# for 2d meshes, we have points and edges.  we use a dict so that we can
+		# name some edges (for boundary conditions later);
+		self.points = [];
+		self.edges = {};
+	def import_3d():
+		# for 3d meshes, we have points and faces.  we use a dict so that we can
+		# name some faces (for boundary conditions later);
+		self.points = [];
+		self.faces = {};
